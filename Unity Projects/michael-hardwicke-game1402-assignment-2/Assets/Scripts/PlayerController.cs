2b21dfb6cd628c8b3d67302218ea69006b43f945
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-02-25 17:11:30 -0700

    Start Assignment 2

---
+++ Unity Projects/michael-hardwicke-game1402-assignment-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float moveSpeed = 2;
+    [SerializeField] private float rotationSpeed = 10;
+    [SerializeField] private float gravity = -9.8f;
+    
+    [Space(10)]
+    [Header("Ground Check")]
+    [SerializeField] private Vector3 groundCheckOffset;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayer;
+    
+    
+    private Vector2 _moveInput;
+    private Vector3 _camForward;
+    private Vector3 _camRight;
+    private Vector3 _moveDirection;
+    private CharacterController _characterController;
+    private Quaternion _targetRotation;
+    private Vector3 _velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        
+    }
+
+    private void CalculateMovement()
+    {
+        _camForward = playerCamera.transform.forward;
+        _camRight = playerCamera.transform.right;
+        _camForward.y = 0;
+        _camRight.y = 0;
+        _camForward.Normalize();
+        _camRight.Normalize();
+
+        _moveDirection = _camRight * _moveInput.x + _camForward * _moveInput.y;
+
+        _targetRotation = Quaternion.LookRotation(_moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, rotationSpeed * Time.deltaTime);
+
+        //Calculate gravity
+        _velocity = _moveDirection * moveSpeed;
+        _velocity.y += gravity * Time.deltaTime;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.SphereCast(
+            transform.position + groundCheckOffset,
+            groundCheckRadius,
+            Vector3.down, 
+            out RaycastHit hit,
+            groundCheckDistance,
+            groundLayer
+            );
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.purple;
+        Gizmos.DrawWireSphere(transform.position + groundCheckOffset, groundCheckRadius);
+        Gizmos.DrawWireSphere(transform.position + groundCheckOffset + Vector3.down * groundCheckDistance, groundCheckRadius);
+        Gizmos.DrawWireCube((transform.position + groundCheckOffset + Vector3.down * groundCheckDistance)/2,
+            new Vector3(2 * groundCheckRadius, groundCheckRadius,2 * groundCheckRadius));
+        
+    }
+}
