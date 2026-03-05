e252bb5da8d4c9b216bf529a4bbb356205c4e13b
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-04 18:12:41 -0700

    Start Arrow Shooting

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/ChestInteractable.cs
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/ChestInteractable.cs
@@ -1,30 +1,58 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class ChestInteractable : MonoBehaviour, IInteractable
 {
    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
    [SerializeField] private Animator anim;
-   
+   private Tween loopTween;
+   private Tween collectTween;
+
+
+   private void Start()
+   {
+      if (!anim) return;
+      
+      transform.DOScale(1.2f, .5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad);
+   }
+
+
    public void OnHoverIn()
    {
-      Debug.Log("Interactor In");
-      anim?.SetBool(IsOpen, true);
-      
-      //TODO - Show UI
-      Toast.Instance.ShowToast("Press \"E\" to Interact");
+      if (anim != null)
+      {
+         Debug.Log("Interactor In");
+         anim?.SetBool(IsOpen, true);
+
+         //TODO - Show UI
+         Toast.Instance.ShowToast("Press \"E\" to Interact");
+      }
    }
-   
+
    public void OnHoverOff()
    {
-      Debug.Log("Interactor Off");
-      anim?.SetBool(IsOpen, false);
-      //TODO - Hide UI
-      Toast.Instance.HideToast();
+      if (anim != null)
+      {
+
+         Debug.Log("Interactor Off");
+         anim?.SetBool(IsOpen, false);
+         //TODO - Hide UI
+         Toast.Instance.HideToast();
+      }
    }
-   
+
    public void OnInteract()
    {
-      Debug.Log($"Interacted with {gameObject.name}");
-      Destroy(gameObject);
+      if (anim != null)
+      {
+         Debug.Log($"Interacted with {gameObject.name}");
+
+         collectTween = transform.DOScale(0, .5f).SetEase(Ease.InBack).OnComplete(() => { Destroy(gameObject); });
+      }
+   }
+
+   void OnDestroy()
+   {
+      DOTween.Kill(gameObject);
    }
 }
