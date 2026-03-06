bc0cab861b2e896762ad985c0c5d0a6fb09e6e4a
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-06 15:26:32 -0700

    Rename CoinPickup

---
+++ UnityProjects/m-h-g1402-ass-2/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour, ICollectable
+{
+    public void OnCollect(GameObject collector)
+    {
+        Debug.Log("Coin Collected");
+        Destroy(gameObject);
+    }
+}
