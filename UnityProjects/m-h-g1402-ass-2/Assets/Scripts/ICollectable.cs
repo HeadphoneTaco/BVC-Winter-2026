9b59c518e14b0ee8aaf31b6a57ec9a4b27bb3a0c
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-03-06 19:10:53 -0700

    Move Interfaces to nested folder

--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/ICollectable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface ICollectable
-{
-    void OnCollect(GameObject collector);
-}
--- UnityProjects/m-h-g1402-ass-2/Assets/Scripts/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IInteractable
-{
-    public void OnHoverIn();
-    public void OnInteract();
-    public void OnHoverOff();
-}
